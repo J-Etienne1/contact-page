4bc129b43849535e40818a619924cfefe9d07a0b
Jason <jasonetienne1@gmail.com>
2022-09-11 17:05:13 +0100

    finished styling

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
-const toggleSwitch = document.querySelector(
-    '.theme-switch input[type="checkbox"]'
-);
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute("data-theme", "dark");
-    } else {
-        document.documentElement.setAttribute("data-theme", "light");
+        document.documentElement.setAttribute('data-theme', 'dark');
     }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
 }
 
-toggleSwitch.addEventListener("change", switchTheme, false);
+toggleSwitch.addEventListener('change', switchTheme, false);
 
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const message = document.getElementById("message");
-const contactForm = document.getElementById("contactForm");
-const errorElement = document.getElementById("errorElement");
-const successMsg = document.getElementById("successMsg");
-const submitBtn = document.getElementById("submitBtn");
 
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const contactForm = document.getElementById('contact-form');
+const errorElement = document.getElementById('error');
+const successMsg = document.getElementById('success-msg');
+const submitBtn = document.getElementById('submit');
+  
 const validate = (e) => {
-    e.preventDefault();
+  e.preventDefault();
+ 
+  if (name.value.length < 3) {
+    errorElement.innerHTML = 'Your name should be at least 3 characters long.';
+    return false;
+  } 
+  
+  if (!(email.value.includes('.') && (email.value.includes('@')))) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  } 
 
-    if (name.vale.length < 3) {
-        errorElement.innerHTML = "Your name should be at least 3 letters long.";
-        return false;
-    }
+  if (!emailIsValid(email.value)) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  }
 
-    if (!(email.ariaValueMax.includes(".") && email.ariaValueMax.includes("@"))) {
-        errorElement.innerHTML = "Please enter a valid email address.";
-        return false;
-    }
+  if (message.value.length < 15) {
+    errorElement.innerHTML = 'Please write a longer message.';
+    return false;
+  }
 
-    if (!emailIsValid(email.value)) {
-        errorElement.innerHTML = "Please enter a valid email address.";
-        return false;
-    }
+  errorElement.innerHTML = '';
+  successMsg.innerHTML = 'Thank you! I will get back to you as soon as possible.'; 
 
-    if (message.value.length < 15) {
-        errorElement.innerHTML = "Please leave a longer message.";
-        return false;
-    }
+  e.preventDefault();
+  setTimeout(function () {
+    successMsg.innerHTML = '';
+    document.getElementById('contact-form').reset();
+  }, 6000);
 
-    errorElement.innerHTML = "";
-    successMsg.innerHTML =
-        "Thank you! We will get back to you as soon as possible.";
+  return true;
 
-    e.preventDefault();
-    setTimeout(function () {
-        successMsg.innerHTML = "";
-        document.getElementById("contact-form").reset();
-    }, 6000);
-
-    return true;
-
-    const emailIsValid = email => {
-        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/test(email);
-    }
-
-    submitBtn.addEventListener('click', validate);
 }
+
+const emailIsValid = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+submitBtn.addEventListener('click', validate);
